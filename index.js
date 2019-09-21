8f805e8e22058e4d2d25233b323d5ef000747edf
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-21 18:10:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 function shout(string) {
-  var uppercase = 'HELLO!'
-  uppercase.toUpperCase() === uppercase // true
   return string.toUpperCase()
 }
 
 function whisper(string) {
-  var lowercase = 'hello!'
-  lowercase.toLowerCase() === lowercase // true
   return string.toLowerCase()
 }
 
 function logShout(string) {
-  var uppercase = 'HELLO!'
-  uppercase.toUpperCase() === uppercase // true
   console.log(string.toUpperCase())
 }
 
