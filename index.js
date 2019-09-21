e44b9609f23953e6bcb1e43d9c0b12a5e3eab187
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-21 17:29:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function whisper(string) {
   var lowercase = 'hello!'
+  lowercase.toLowerCase() === lowercase // true
   return string.lowercase.toLowerCase()
 }
 
 function logShout(string) {
-  
+  var uppercase = 'HELLO!'
+  uppercase.toUpperCase() === uppercase // true
+  console.log(logShout)
 }
